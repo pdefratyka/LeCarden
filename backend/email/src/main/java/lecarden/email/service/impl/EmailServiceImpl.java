bfc395ac3a8ba77c8f4af4146e777dc2f03d052a
Paweł Defratyka <pawel.defratyka@capgemini.com>
2020-02-05 15:58:39 +0100

    V1-R0.9B-Example-Of-Email-With-Template-To-Deep-Refactor

--- backend/email/src/main/java/lecarden/email/service/impl/EmailServiceImpl.java
+++ backend/email/src/main/java/lecarden/email/service/impl/EmailServiceImpl.java
@@ -6,23 +6,36 @@ import lecarden.email.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
     private JavaMailSender javaMailSender;
+    private ITemplateEngine templateEngine;
+    private MimeMessage mail;
 
     @Autowired
-    public EmailServiceImpl(JavaMailSender javaMailSender){
+    public EmailServiceImpl(JavaMailSender javaMailSender, ITemplateEngine templateEngine){
         this.javaMailSender=javaMailSender;
+        this.templateEngine=templateEngine;
     }
 
     @Override
     public void sendConfirmationRegisterEmail(User user) {
-        final String topic="Lecarden Confirmation";
-        final String body="Hallo "+user.getLogin();
-        javaMailSender.send(setMailProperties(new Email(user.getEmail(),topic,body)));
+
+        try {
+            buildMail(user).setText(processTemplate(), true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        javaMailSender.send(this.mail);
     }
 
     private SimpleMailMessage setMailProperties(Email email){
@@ -31,5 +44,25 @@ public class EmailServiceImpl implements EmailService {
         msg.setSubject(email.getTopic());
         msg.setText(email.getBody());
         return msg;
+    }
+
+    private MimeMessageHelper buildMail(User user) {
+        final String topic="Lecarden Confirmation";
+        final String body="Hallo "+user.getLogin();
+        this.mail = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+        try {
+            helper = new MimeMessageHelper(mail, true);
+            helper.setTo(user.getEmail());
+            helper.setSubject(topic);
+            helper.setText(body);
+        } catch (MessagingException e) {
+        }
+        return helper;
+    }
+
+    private String processTemplate() {
+        Context context = new Context();
+        return templateEngine.process("registration_confirmation", context);
     }
 }
