bfc290d0e22e4aa2ae090b508359ee2365d74870
pdefratyka <p.defratyka28@gmail.com>
2020-09-24 21:45:31 +0200

    Refactor of learning mode and learning mode's children components

--- frontend/lecarden/src/app/feature/learning/learning-mode/basket-mode-selector/basket-mode-selector.component.ts
+++ frontend/lecarden/src/app/feature/learning/learning-mode/basket-mode-selector/basket-mode-selector.component.ts
@@ -1,17 +1,20 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-basket-mode-selector',
   templateUrl: './basket-mode-selector.component.html',
-  styleUrls: ['./basket-mode-selector.component.scss'],
+  styleUrls: [
+    './../../../../shared/styles/global.scss',
+    './basket-mode-selector.component.scss',
+  ],
 })
 export class BasketModeSelectorComponent {
   @Output()
   basketMode: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input()
   isBasketModeSelected = false;
 
   emitBasketMode(): void {
-    this.isBasketModeSelected = !this.isBasketModeSelected;
     this.basketMode.emit(this.isBasketModeSelected);
   }
 }
--- frontend/lecarden/src/app/feature/learning/learning-mode/learning-mode-selector/learning-mode-selector.component.ts
+++ frontend/lecarden/src/app/feature/learning/learning-mode/learning-mode-selector/learning-mode-selector.component.ts
@@ -1,10 +1,13 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { LearningMode } from 'src/app/shared/models/learningMode';
 
 @Component({
   selector: 'app-learning-mode-selector',
   templateUrl: './learning-mode-selector.component.html',
-  styleUrls: ['./learning-mode-selector.component.scss'],
+  styleUrls: [
+    './../../../../shared/styles/global.scss',
+    './learning-mode-selector.component.scss',
+  ],
 })
 export class LearningModeSelectorComponent {
   LearningMode = LearningMode;
@@ -14,7 +17,9 @@ export class LearningModeSelectorComponent {
   selectMode: EventEmitter<LearningMode> = new EventEmitter<LearningMode>();
 
   emitSelectMode(mode: LearningMode): void {
-    this.selectedMode = mode;
+    if (mode === this.selectedMode) {
+      mode = null;
+    }
     this.selectMode.emit(mode);
   }
 
